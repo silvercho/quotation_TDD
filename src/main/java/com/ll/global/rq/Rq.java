a253d87d931661fdcce2f031bec30a3f43dfa1c6
silvercho <tjddms01212@gmail.com>
2023-11-13 17:27:23 +0900

    RqTest t3 pass

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -1,11 +1,25 @@
 package com.ll.global.rq;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     private final String action;
+    private final String queryString;
+    private Map<String, String> params;
 
     public Rq(final String cmd) {
         final String[] cmdBits = cmd.split("\\?",2);
         action = cmdBits[0].trim();
+        queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
+        params = new HashMap<>();
+
+        if ( queryString.isBlank()) return;
+
+        final String[] paramBits = queryString.split("=",2);
+        final String paramName = paramBits[0].trim();
+        final String paramValue = paramBits[1].trim();
+        params.put(paramName,paramValue);
     }
 
     public String getAction() {
@@ -13,6 +27,6 @@ public class Rq {
     }
 
     public String getParameter(String paramName) {
-        return "Paul";
+        return params.get(paramName);
     }
 }
