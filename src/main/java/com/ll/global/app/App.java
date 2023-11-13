fda87ef74b9328314aeec95931f69ecfae7670ef
silvercho <tjddms01212@gmail.com>
2023-11-13 16:23:43 +0900

    t7 pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -1,5 +1,9 @@
 package com.ll.global.app;
 
+import com.ll.global.domain.quotation.quotation.entity.Quotation;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,7 +15,11 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        List<Quotation> quotations = new ArrayList<>();
+
         long lastQuotationId = 0;
+
         while (true) {
             final String cmd = scanner.nextLine().trim();
 
@@ -23,13 +31,28 @@ public class App {
 
                 final long id = ++lastQuotationId;
 
+                Quotation quotation = new Quotation(id,authorName,content);
+                quotations.add(quotation);
+
+
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
             } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
-                System.out.println("1 / 작자미상 / 현재를 사랑하라.");
-            } else if (cmd.equals("종료")) return;
+
+                quotations
+                        .reversed()
+                        .forEach(
+                                quotation -> System.out.println(
+                                        "%d / %s / %s".formatted(
+                                                quotation.getId(),
+                                                quotation.getAuthorName(),
+                                                quotation.getContent()
+                                        )
+                                )
+                        );
+                } else if (cmd.equals("종료")) return;
+            }
         }
     }
-}
+
