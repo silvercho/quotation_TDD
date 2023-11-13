2b6c5717203e94bb00b98b2fc1de8d381310c9e6
silvercho <tjddms01212@gmail.com>
2023-11-13 11:41:51 +0900

    t3_2 pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -11,7 +11,10 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
         System.out.print("명언 : ");
         System.out.print("작가 : ");
+
+        System.out.println("1번 명언이 등록되었습니다.");
     }
 }
