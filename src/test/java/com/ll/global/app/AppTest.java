8bd0ce8cca6b47ac6854926b6b56ab850e9f725f
silvercho <tjddms01212@gmail.com>
2023-11-13 15:35:08 +0900

    t4 refac, t5&& pass

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AppTest {
 
     private String run(final String  cmd){
-        final  Scanner scanner = TestUtil.genScanner(cmd.stripIndent().trim());
+        final  Scanner scanner = TestUtil.genScanner(cmd.stripIndent().trim()+ "\n종료");
 
         final  ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
@@ -27,9 +27,7 @@ public class AppTest {
     @Test
     @DisplayName("프로그램 시작 시 \"== 명언 앱 ==\" 출력")
     void t1(){
-            final String out = run("""
-                종료
-                """);
+            final String out = run("");
 
             assertThat(out)
                     .contains("== 명언 앱 ==");
@@ -38,9 +36,7 @@ public class AppTest {
     @Test
     @DisplayName("종료")
     void t2() {
-        final String out = run("""
-                종료
-                """);
+        final String out = run("");
     }
     @Test
     @DisplayName("등록")
@@ -49,7 +45,6 @@ public class AppTest {
               등록
                 현재를 사랑하라.
                 작자미상
-                종료
                 """);
 
         assertThat(out)
@@ -58,30 +53,31 @@ public class AppTest {
                 .contains("1번 명언이 등록되었습니다.");
     }
     @Test
-    @DisplayName("등록")
+    @DisplayName("등록할 때마다 번호가 1씩 증가")
     void t4() {
         final String out = run("""
-              등록
+                등록
                 현재를 사랑하라.
                 작자미상
-                종료
-                """);
+                  """);
 
         assertThat(out)
                 .contains("1번 명언이 등록되었습니다.")
                 .doesNotContain("2번 명언이 등록되었습니다.");
-
-        final String out2 = run("""
+    }
+    @Test
+    @DisplayName("등록할 때마다 번호가 1씩 증가, 2건 등록")
+    void t5() {
+        final String out = run("""
                등록
                현재를 사랑하라.
                작자미상
                등록
                현재를 사랑하라.
                작자미상
-               종료
                """);
 
-        assertThat(out2)
+        assertThat(out)
                 .contains("1번 명언이 등록되었습니다.")
                 .contains("2번 명언이 등록되었습니다.")
                 .doesNotContain("3번 명언이 등록되었습니다.");
