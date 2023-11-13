eec8c2fdf3d05b57e6fa9684a98c1ac6fa6192a5
silvercho <tjddms01212@gmail.com>
2023-11-13 10:47:44 +0900

    t3()

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -45,6 +45,26 @@ public class AppTest {
 
         String out = byteArrayOutputStream.toString().trim();
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+    }
+    @Test
+    @DisplayName("등록")
+    void t3() {
+        Scanner scanner = TestUtil.genScanner("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """.stripIndent());
 
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App(scanner).run();
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out)
+                .contains("명언 : ")
+                .contains("작가 : ");
     }
 }
